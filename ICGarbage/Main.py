a39fdfe9efd032068453c3b0c95ef4167619d7dd
dulda280 <Sebber280@live.dk>
2024-02-06 10:49:44 +0100

    Refactored old project

--- ICGarbage/Main.py
+++ ICGarbage/Main.py
@@ -48,41 +48,13 @@ def draw_flow(img, flow, step=16):
     return img_bgr
 
 
-# Optical flow function that maps opencv optical flow function outputs to hsv values for a visual image.
-def draw_hsv(flow):
-    h, w = flow.shape[:2]
-    fx, fy = flow[:, :, 0], flow[:, :, 1]
-
-    ang = np.arctan2(fy, fx) + np.pi
-    v = np.sqrt(fx * fx + fy * fy)
-
-    hsv = np.zeros((h, w, 3), np.uint8)
-    hsv[..., 0] = ang * (180 / np.pi / 2)
-    hsv[..., 1] = 255
-    hsv[..., 2] = np.minimum(v * 4, 255)
-    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-    return bgr
-
-
 if __name__ == '__main__':
     # ---VARIABLES---#
     frameCount = 0
-    calibrating = True
     check = True
     cap = cv2.VideoCapture('GL010031.mp4')
     ret, frame = cap.read()
-    closing = False
-    closeCounter = 0
-    closeTimer = 0
-    movement = False
-    movementTimer = 0
-    stillClosedBool = False
-    imNum = 0
     fiftyFrame = []
-    minBoundingVal = 0
-    fakeCloseCounter = 0
-    properties = ['area', 'bbox', 'bbox_area']
     ins = instanceSegmentation()
     we = WasteExtraction()
     ins.load_model("pointrend_resnet50.pkl", confidence=0.2)
@@ -108,121 +80,13 @@ if __name__ == '__main__':
                     rightYTop1, rightXTop1, rightYBottom1, rightXBottom1 = right_bbox
                     check = False
 
-                # Making ROI's again, this time in the while loop so it keeps updating, and not just doing it onee as once again, we need it for the
-                # optical flow function
-                roiLeft = left[leftYTop1:leftYBottom1 + 1, leftXTop1:leftXBottom1 + 1]
-                roiRight = right[rightYTop1:rightYBottom1 + 1, rightXTop1:rightXBottom1 + 1]
-
-                # New maskoff, only difference is this is agai in the while loop, so it keeps updating.
-                maskedRight = maskOff(roiRight, erodeF2)
-                # Gray version again, needed for optical flow
-                maskedRightGray = cv2.cvtColor(maskedRight, cv2.COLOR_BGR2GRAY)
-
-                # The optical flow function! Uses gray images of the current frame and the frame from just before
-                flow2 = cv2.calcOpticalFlowFarneback(maskedRightPrevGray, maskedRightGray, None, 0.5, 3, 25, 3, 5, 1.2,
-                                                     0)
-                # Sets the previous frame to the current so it is ready for the next frame.
-                maskedRightPrevGray = maskedRightGray
-
-                # Makes the hsv representation of the optical flow.
-                flowHSVRight = draw_hsv(flow2)
-
-                # Makes binary image of the hsv image of the optical flow
-                flowThreshRight = cv2.inRange(flowHSVRight, (0, 0, 5), (180, 255, 255))
-                # Blob analysis again to find the biggest blob
-                blobsRight = label(flowThreshRight > 0)
-                cv2.imshow("optical treshold", flowThreshRight)
-                dfRight = pd.DataFrame(regionprops_table(blobsRight, properties=properties))
-                # If the biggest blob is over 400, the grabbers are moving!!
-                if len(dfRight) != 0 and max(dfRight['area']) > 400:
-                    closing = True
-                    closeCounter += 1
-                    print(closeCounter)
-
-                # A bunch of if statements that checks whether we got a fake closing detection or not
-                if closing:
-                    closeTimer += 1
-                    movementTimer = 0
-                    # if it only makes 2 or less detection from the first detection after 10 frames, save as false detection. Also resets detection variables
-                    if closeTimer > 10 and closeCounter <= 3 and stillClosedBool == False:
-                        closing = False
-                        closeCounter = 0
-                        closeTimer = 0
-                        # print("fake close")
-                        fakeCloseCounter += 1
-
-                        print("Fake Close Counter: " + str(fakeCloseCounter))
-                    # If it makes 3 or more detection in the last 10 frames from the first detection save as correct detection. Also resets detection variables
-                    if closeTimer > 10 and closeCounter > 3 and stillClosedBool == False and len(fiftyFrame) > 495:
-                        closing = False
-                        closeCounter = 0
-                        closeTimer = 0
-
-                        print("We closing!")
-                        # Save image from 70 frames ago as picture of garbage. Makes ROI of the image, to filter out unnecessary noise.
-
-                        saveImg = fiftyFrame[500 - 70]
-                        saveImgRoi = saveImg[0:saveImg.shape[0],
-                                     leftXBottom1:int(rightXTop1 + (saveImg.shape[1] / 2))]
-                        resize = cv2.resize(saveImgRoi, (250, 250))
-                        # Save the chosen image on the pc.
-                        cv2.imwrite("savedImg/garbage" + str(imNum) + ".png", resize)
-                        # The code line for the neural network segmentation of the image.
-                        results, output = ins.segmentImage("savedImg/garbage" + str(imNum) + ".png",
-                                                           show_bboxes=True,
-                                                           output_image_name="savedImg/segmented" + str(
-                                                               imNum) + ".png")
-                        # Goes through all the bounding boxes that is found by the NN on the image and chooses the one closest to the grabbers position.
-                        for i in range(0, len(results['boxes'])):
-                            boxWidth = results['boxes'][i][2] - results['boxes'][i][0]
-                            boxHeight = results['boxes'][i][3] - results['boxes'][i][1]
-                            boxArea = boxWidth * boxHeight
-                            if boxArea > minBoundingVal:
-                                minBoundingVal = boxArea
-                                print("val: " + str(minBoundingVal))
-                                print(imNum)
-                                bbx1 = results['boxes'][i][0]
-                                bby1 = results['boxes'][i][1]
-                                bbx2 = results['boxes'][i][2]
-                                bby2 = results['boxes'][i][3]
-                        # Draws the chosen bounding box on a new image and saves it on the pc.
-                        if len(results['boxes']) != 0:
-                            centerX = (bbx1 + bbx2) / 2
-                            centerY = (bby1 + bby2) / 2
-                            width = bbx2 - bbx1
-                            height = bby2 - bby1
-                            ncenterX = centerX / resize.shape[1]
-                            ncenterY = centerY / resize.shape[0]
-                            nwidth = width / resize.shape[1]
-                            nheight = height / resize.shape[0]
-
-                            cv2.rectangle(resize, (bbx1, bby1), (bbx2, bby2), (0, 255, 0), thickness=2)
-                            cv2.imwrite("savedImg/bbox" + str(imNum) + ".png", resize)
-                            f1 = open("yolotxt/" + str(imNum) + ".txt", "w+")
-                            f1.write(
-                                "Class here " + str(ncenterX) + " " + str(ncenterY) + " " + str(nwidth) + " " + str(
-                                    nheight))
-                            f1.close()
-                            # Resets the minBoundingVal variable so it is ready for a new image segmentation. Also sets movement to tru to start the timer.
-                            minBoundingVal = 1000
-                            imNum += 1
-                        movement = True
-                # If correct detection happens, start a timer that resets everytime a detection happens after the first one,
-                # until 100 frames passes without a new detection, so we know that the grabbers have closed
-                if movement:
-                    closing = False
-                    stillClosedBool = True
-                    movementTimer += 1
-
-                # if the timer goes over 100 the grabbers have closed and we can start detecting for movement again.
-                if movementTimer > 80:
-                    closeCounter = 0
-                    movement = False
-                    stillClosedBool = False
+                dfRight, new_prev_gray = we.opticalFlowHSV(right_img=right, right_bbox=right_bbox, erodeF2=erodeF2, maskedRightPrevGray=maskedRightPrevGray)
+                maskedRightPrevGray = new_prev_gray
+                we.closing_event_handler(dfRight=dfRight, fiftyFrame=fiftyFrame, leftBottomX1=leftXBottom1, rightTopX1=rightXTop1, instance_segmenter=ins)
 
                 # shows some images
-                cv2.imshow('hsvtreshRight', flowThreshRight)
-                cv2.imshow('flowRight', draw_flow(maskedRightGray, flow2))
+                #cv2.imshow('hsvtreshRight', flowThreshRight)
+                #cv2.imshow('flowRight', draw_flow(maskedRightGray, flow2))
 
         if cv2.waitKey(1) == ord('s'):
             break
--- ICGarbage/WasteExtraction.py
+++ ICGarbage/WasteExtraction.py
@@ -15,6 +15,16 @@ class WasteExtraction:
         self.rightCnts = []
         self.contourVal = contourval
         self.kernel1 = np.ones((5, 5), np.uint8)
+        self.closing = False
+        self.closeCounter = 0
+        self.closeTimer = 0
+        self.movementTimer = 0
+        self.stillClosedBool = False
+        self.fakeCloseCounter = 0
+        self.movement = False
+        self.imNum = 0
+        self.minBoundingVal = 0
+
 
     def getROI(self, currentFrame):
         leftRegionIMG = currentFrame[int(currentFrame.shape[0] / 2):currentFrame.shape[0],
@@ -178,3 +188,143 @@ class WasteExtraction:
         left_bbox = [leftYTop1, leftXTop1, leftYBottom1, leftXBottom1]
         right_bbox = [rightYTop1, rightXTop1, rightYBottom1, rightXBottom1]
         return left_bbox, right_bbox, erodeF2, maskedRightPrevGray
+
+    # Optical flow function that maps opencv optical flow function outputs to hsv values for a visual image.
+    def draw_hsv(sef, flow):
+        h, w = flow.shape[:2]
+        fx, fy = flow[:, :, 0], flow[:, :, 1]
+
+        ang = np.arctan2(fy, fx) + np.pi
+        v = np.sqrt(fx * fx + fy * fy)
+
+        hsv = np.zeros((h, w, 3), np.uint8)
+        hsv[..., 0] = ang * (180 / np.pi / 2)
+        hsv[..., 1] = 255
+        hsv[..., 2] = np.minimum(v * 4, 255)
+        bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+        return bgr
+
+    def opticalFlowHSV(self, right_img, right_bbox, erodeF2, maskedRightPrevGray):
+        # Making ROI's again, this time in the while loop so it keeps updating, and not just doing it onee as once again, we need it for the
+        # optical flow function
+        rightYTop1, rightXTop1, rightYBottom1, rightXBottom1 = right_bbox
+        roiRight = right_img[rightYTop1:rightYBottom1 + 1, rightXTop1:rightXBottom1 + 1]
+
+        # New maskoff, only difference is this is agai in the while loop, so it keeps updating.
+        maskedRight = self.maskOff(roiRight, erodeF2)
+        # Gray version again, needed for optical flow
+        maskedRightGray = cv2.cvtColor(maskedRight, cv2.COLOR_BGR2GRAY)
+
+        # The optical flow function! Uses gray images of the current frame and the frame from just before
+        flow2 = cv2.calcOpticalFlowFarneback(maskedRightPrevGray, maskedRightGray, None, 0.5, 3, 25, 3, 5, 1.2,
+                                             0)
+        # Sets the previous frame to the current so it is ready for the next frame.
+        maskedRightPrevGray = maskedRightGray
+
+        # Makes the hsv representation of the optical flow.
+        flowHSVRight = self.draw_hsv(flow2)
+
+        # Makes binary image of the hsv image of the optical flow
+        flowThreshRight = cv2.inRange(flowHSVRight, (0, 0, 5), (180, 255, 255))
+        # Blob analysis again to find the biggest blob
+        blobsRight = label(flowThreshRight > 0)
+        cv2.imshow("optical treshold", flowThreshRight)
+        dfRight = pd.DataFrame(regionprops_table(blobsRight, properties=self.properties))
+
+        return dfRight, maskedRightPrevGray
+
+    def is_closing_over(self):
+        if self.movement:
+            self.closing = False
+            self.stillClosedBool = True
+            self.movementTimer += 1
+
+        # if the timer goes over 100 the grabbers have closed and we can start detecting for movement again.
+        if self.movementTimer > 80:
+            self.closeCounter = 0
+            self.movement = False
+            self.stillClosedBool = False
+
+    def get_waste_frame(self, fiftyFrame, leftXBottom1, rightXTop1, instance_segmenter):
+        print("We closing!")
+        # Save image from 70 frames ago as picture of garbage. Makes ROI of the image, to filter out unnecessary noise.
+
+        saveImg = fiftyFrame[500 - 70]
+        saveImgRoi = saveImg[0:saveImg.shape[0],
+                     leftXBottom1:int(rightXTop1 + (saveImg.shape[1] / 2))]
+        resize = cv2.resize(saveImgRoi, (250, 250))
+        # Save the chosen image on the pc.
+        cv2.imwrite("savedImg/garbage" + str(self.imNum) + ".png", resize)
+        # The code line for the neural network segmentation of the image.
+        results, output = instance_segmenter.segmentImage("savedImg/garbage" + str(self.imNum) + ".png",
+                                           show_bboxes=True,
+                                           output_image_name="savedImg/segmented" + str(
+                                               self.imNum) + ".png")
+        # Goes through all the bounding boxes that is found by the NN on the image and chooses the one closest to the grabbers position.
+        for i in range(0, len(results['boxes'])):
+            boxWidth = results['boxes'][i][2] - results['boxes'][i][0]
+            boxHeight = results['boxes'][i][3] - results['boxes'][i][1]
+            boxArea = boxWidth * boxHeight
+            if boxArea > self.minBoundingVal:
+                self.minBoundingVal = boxArea
+                print("val: " + str(self.minBoundingVal))
+                print(self.imNum)
+                bbx1 = results['boxes'][i][0]
+                bby1 = results['boxes'][i][1]
+                bbx2 = results['boxes'][i][2]
+                bby2 = results['boxes'][i][3]
+        # Draws the chosen bounding box on a new image and saves it on the pc.
+        if len(results['boxes']) != 0:
+            centerX = (bbx1 + bbx2) / 2
+            centerY = (bby1 + bby2) / 2
+            width = bbx2 - bbx1
+            height = bby2 - bby1
+            ncenterX = centerX / resize.shape[1]
+            ncenterY = centerY / resize.shape[0]
+            nwidth = width / resize.shape[1]
+            nheight = height / resize.shape[0]
+
+            cv2.rectangle(resize, (bbx1, bby1), (bbx2, bby2), (0, 255, 0), thickness=2)
+            cv2.imwrite("savedImg/bbox" + str(self.imNum) + ".png", resize)
+            f1 = open("yolotxt/" + str(self.imNum) + ".txt", "w+")
+            f1.write(
+                "Class here " + str(ncenterX) + " " + str(ncenterY) + " " + str(nwidth) + " " + str(
+                    nheight))
+            f1.close()
+            # Resets the minBoundingVal variable so it is ready for a new image segmentation. Also sets movement to tru to start the timer.
+            self.minBoundingVal = 1000
+            self.imNum += 1
+        self.movement = True
+    def closing_event_handler(self, dfRight, fiftyFrame, leftBottomX1, rightTopX1, instance_segmenter):
+        # If the biggest blob is over 400, the grabbers are moving!!
+        if len(dfRight) != 0 and max(dfRight['area']) > 400:
+            self.closing = True
+            self.closeCounter += 1
+            print(self.closeCounter)
+
+        # A bunch of if statements that checks whether we got a fake closing detection or not
+        if self.closing:
+            self.closeTimer += 1
+            self.movementTimer = 0
+            # if it only makes 2 or less detection from the first detection after 10 frames, save as false detection. Also resets detection variables
+            if self.closeTimer > 10 and self.closeCounter <= 3 and self.stillClosedBool == False:
+                self.closing = False
+                self.closeCounter = 0
+                self.closeTimer = 0
+                # print("fake close")
+                self.fakeCloseCounter += 1
+
+                print("Fake Close Counter: " + str(self.fakeCloseCounter))
+            # If it makes 3 or more detection in the last 10 frames from the first detection save as correct detection. Also resets detection variables
+            if self.closeTimer > 10 and self.closeCounter > 3 and self.stillClosedBool == False and len(fiftyFrame) > 495:
+                self.closing = False
+                self.closeCounter = 0
+                self.closeTimer = 0
+
+                print("We closing!")
+                # Save image from 70 frames ago as picture of garbage. Makes ROI of the image, to filter out unnecessary noise.
+
+                self.get_waste_frame(fiftyFrame, leftBottomX1, rightTopX1, instance_segmenter=instance_segmenter)
+
+        self.is_closing_over()
