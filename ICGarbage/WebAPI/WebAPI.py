824e89d591d0bc07df7d83b5c7692fe454e13241
dulda280 <Sebber280@live.dk>
2024-02-19 15:16:07 +0100

    web app handles chunk uploads and web api ready for task handling

--- ICGarbage/WebAPI/WebAPI.py
+++ ICGarbage/WebAPI/WebAPI.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import urllib3
+
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route("/")
 def hello_world():
@@ -12,25 +14,25 @@ def hello_world():
 
 @app.route('/uploadvid', methods=['POST'])
 def upload_file():
+    upload_done = False
     UPLOAD_FOLDER = 'uploads'
     if 'videochunk' not in request.files:
         return jsonify({'error': 'No file part'}), 400
 
     file = request.files['videochunk']
     print(file.filename)
-    print(file.name)
-    print(file.headers)
-    print(file.stream)
-    print(file.content_type)
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
     chunk = request.files['videochunk'].read()
-    filename = file.filename
-    chunk_num = int(request.args.get('chunk', 0))
-    total_chunks = int(request.args.get('total_chunks', 1))
-    print("chunk num", chunk_num)
-    print("total_chunks", total_chunks)
+    info_string = file.filename.split(',')
+    filename = info_string[0]
+    print("filename: ", filename)
+    print("chunknum: ", info_string[1])
+    print("total: ", info_string[2])
+    print(info_string)
+    chunk_num = int(info_string[1].split(':')[1].strip())
+    total_chunks = int(info_string[2].split(':')[1].strip())
     # Create a file if it does not exist and append chunks
     with open(os.path.join(UPLOAD_FOLDER, filename), 'ab') as f:
         f.write(chunk)
@@ -39,10 +41,13 @@ def upload_file():
     if chunk_num == total_chunks - 1:
         final_filename = os.path.join(UPLOAD_FOLDER, filename)
         os.rename(os.path.join(UPLOAD_FOLDER, filename), final_filename)
+        upload_done = True
         return jsonify({'message': 'File uploaded successfully',
-                        'filename': final_filename}), 200
+                        'filename': final_filename, 'chunk_num': chunk_num,
+                        'total_chunks': total_chunks, 'upload_done': upload_done}), 200
 
-    return jsonify({'message': f'Chunk {chunk_num + 1} of {total_chunks} uploaded successfully'}), 200
+
+    return jsonify({'chunk_num': chunk_num, 'total_chunks': total_chunks, 'upload_done': upload_done}), 200
 
 
 if __name__ == '__main__':
