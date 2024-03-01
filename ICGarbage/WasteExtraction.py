28191b3143ce1bfb3b836f783a707ea63c2e49c1
tromborg <64638583+tromborg@users.noreply.github.com>
2024-03-01 12:57:14 +0100

    Final adjustments to waste exstractor

--- ICGarbage/WasteExtraction.py
+++ ICGarbage/WasteExtraction.py
@@ -33,8 +33,8 @@ class WasteExtraction:
         print("We closing!")
         # Save image from 70 frames ago as picture of garbage. Makes ROI of the image, to filter out unnecessary noise.
         screenSize = fiftyFrame[0].shape[1] / 3
-        imageArray = fiftyFrame[:-40]
-        grabberXArrayminus40 = grabberXArray[:-40]
+        imageArray = fiftyFrame
+        grabberXArrayminus40 = grabberXArray
         yoloResults = model(imageArray, conf=0.4)
         frameNumber = 0
         validResults = []
@@ -43,17 +43,18 @@ class WasteExtraction:
         validPairs = []
         resultsArray = []
         finalImage = None
+
         for result in yoloResults:
             if len(result.boxes) > 0:
                 if int(result.boxes.xyxy[0][0]) < leftbbox[2] and int(result.boxes.xyxy[0][3]) > leftbbox[1]:
-                    print("grabberx: ", grabberXArrayminus40[frameNumber], "startgrabberX: ",grabberStartX)
+                    print("leftX: ", int(result.boxes.xyxy[0][0]), "leftY: ", int(result.boxes.xyxy[0][3]))
                     frameNumber += 1
                     continue
-                if int(result.boxes.xyxy[0][2]) > rightbbox[0] and int(result.boxes.xyxy[0][3] > rightbbox[1]):
-                    print("grabberx: ", grabberXArrayminus40[frameNumber], "startgrabberX: ", grabberStartX)
+                if int(result.boxes.xyxy[0][2]) > rightbbox[0] and int(result.boxes.xyxy[0][3]) > rightbbox[1]:
+                    print("rightX: ", int(result.boxes.xyxy[0][2]), "rightY: ", int(result.boxes.xyxy[0][3]))
                     frameNumber += 1
                     continue
-                if grabberXArrayminus40[frameNumber] - 40 > grabberStartX:
+                if grabberXArrayminus40[frameNumber] - 30 > grabberStartX:
                     print("grabberxareray: ", grabberXArrayminus40[frameNumber])
                     frameNumber += 1
                     continue
@@ -64,6 +65,7 @@ class WasteExtraction:
                     validFrames.append(fiftyFrame[frameNumber])
             frameNumber += 1
         if len(validResults) > 0:
+            print("length: ", len(validResults))
             for i in range(0,len(validResults)):
                 boxWidth = int(validPairs[i][0].boxes.xyxy[0][2]) - int(validPairs[i][0].boxes.xyxy[0][0])
                 boxHeight = int(validPairs[i][0].boxes.xyxy[0][3]) - int(validPairs[i][0].boxes.xyxy[0][1])
