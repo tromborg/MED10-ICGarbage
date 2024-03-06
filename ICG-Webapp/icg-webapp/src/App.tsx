401dcd6e1587d09030438c009af079546d1a5fe1
tromborg <64638583+tromborg@users.noreply.github.com>
2024-03-06 10:23:04 +0100

    Login Routing

--- ICG-Webapp/icg-webapp/src/App.tsx
+++ ICG-Webapp/icg-webapp/src/App.tsx
@@ -6,13 +6,15 @@ import Users from "./pages/Users";
 import RootLayout from "./layouts/RootLayout";
 import LoginPage from "./components/UserComponents/LoginPage";
 import SignupPage from "./components/UserComponents/SignupPage";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { RootState } from "./store/store";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isLoggedIn = useSelector((state: RootState) => state.login.isLoggedIn);
+
   return (
-    <Provider store={store}>
-      <Routes>
+    <Routes>
+      {!isLoggedIn ? (
         <Route path="/" element={<RootLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
@@ -20,8 +22,14 @@ function App() {
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignupPage />} />
         </Route>
-      </Routes>
-    </Provider>
+      ) : (
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="users" element={<Users />} />
+        </Route>
+      )}
+    </Routes>
   );
 }
 
--- ICG-Webapp/icg-webapp/src/components/Navbar.tsx
+++ ICG-Webapp/icg-webapp/src/components/Navbar.tsx
@@ -3,41 +3,61 @@ import { NavLink } from "react-router-dom";
 import themes from "../design/themes";
 import logo from "../design/logo/png/logo-no-background.png";
 import urls from "../components/urls";
+import { RootState } from "../store/store";
+import { useSelector } from "react-redux";
 
 const NavButtonStyle = {
-    backgroundcolor: themes.adobePalette.darker,
-    ':hover': {
-        color: themes.adobePalette.lighter,
-        backgroundcolor: themes.adobePalette.darkest,
-    },
-    fontSize: "25"
-}
+  backgroundcolor: themes.adobePalette.darker,
+  ":hover": {
+    color: themes.adobePalette.lighter,
+    backgroundcolor: themes.adobePalette.darkest,
+  },
+  fontSize: "25",
+};
 
 const Navbar = () => {
-    return (
-        <Flex as="nav" justify={"space-around"} wrap={'wrap'} gap={'2'} alignItems={'center'} backgroundColor={themes.primaryColours.background}>
-            <NavLink to={urls.home}>
-                <Box>
-                    <Image src={logo} boxSize={"6em"} />
-                </Box>
-            </NavLink>
-            <HStack gap={"2em"}>
-                <NavLink to={urls.home}>
-                    <Box sx={NavButtonStyle}> Home </Box>
-                </NavLink>
-                <NavLink to={urls.about}>
-                    <Box sx={NavButtonStyle}> About </Box>
-                </NavLink>
-                <NavLink to={urls.users}>
-                    <Box sx={NavButtonStyle}> Users </Box>
-                </NavLink>
-                <NavLink to={urls.login}>
-                    <Box sx={NavButtonStyle}>Login</Box>
-                </NavLink>
-            </HStack>
-        </Flex>
-       
+  const isLoggedIn = useSelector((state: RootState) => state.login.isLoggedIn);
 
-    )
-}
+  return (
+    <Flex
+      as="nav"
+      justify={"space-around"}
+      wrap={"wrap"}
+      gap={"2"}
+      alignItems={"center"}
+      backgroundColor={themes.primaryColours.background}
+    >
+      <NavLink to={urls.home}>
+        <Box>
+          <Image src={logo} boxSize={"6em"} />
+        </Box>
+      </NavLink>
+      {!isLoggedIn ? (
+        <HStack gap={"2em"}>
+          <NavLink to={urls.home}>
+            <Box sx={NavButtonStyle}> Home </Box>
+          </NavLink>
+          <NavLink to={urls.about}>
+            <Box sx={NavButtonStyle}> About </Box>
+          </NavLink>
+          <NavLink to={urls.login}>
+            <Box sx={NavButtonStyle}>Login</Box>
+          </NavLink>
+        </HStack>
+      ) : (
+        <HStack gap={"2em"}>
+          <NavLink to={urls.home}>
+            <Box sx={NavButtonStyle}> Home </Box>
+          </NavLink>
+          <NavLink to={urls.about}>
+            <Box sx={NavButtonStyle}> About </Box>
+          </NavLink>
+          <NavLink to={urls.users}>
+            <Box sx={NavButtonStyle}> Users </Box>
+          </NavLink>
+        </HStack>
+      )}
+    </Flex>
+  );
+};
 export default Navbar;
